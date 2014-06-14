cdf2a15920b1edfbbd98fe899eba00ec24885ef0
Shani-08 <ShahjahanDurrani@hotmail.com>
2014-06-14 22:46:33 +0100

    rewrite re.sub function
    too slow for \b\b replacements

--- other/jsunpackMM.py
+++ other/jsunpackMM.py
@@ -22,7 +22,7 @@ import re
 
 def unpack(sJavascript,iteration=1, totaliterations=3  ):
 
-
+    print 'iteration',iteration
     aSplit = sJavascript.split("rn p}('")
     
     p1,a1,c1,k1=('','0','0','')
@@ -56,7 +56,7 @@ def unpack(sJavascript,iteration=1, totaliterations=3  ):
     d = ''#32823
 
     #sUnpacked = str(__unpack(p, a, c, k, e, d))
-    sUnpacked1 = str(__unpack(p1, a1, c1, k1, e, d))
+    sUnpacked1 = str(__unpack(p1, a1, c1, k1, e, d,iteration))
     
     #print sUnpacked[:200]+'....'+sUnpacked[-100:], len(sUnpacked)
 #    print sUnpacked1[:200]+'....'+sUnpacked1[-100:], len(sUnpacked1)
@@ -69,14 +69,52 @@ def unpack(sJavascript,iteration=1, totaliterations=3  ):
 #        print 'final res for this iteration is',iteration
         return unpack(sUnpacked1,iteration+1)#.replace('\\', ''),iteration)#.replace('\\', '');#unpack(sUnpacked.replace('\\', ''))
 
-def __unpack(p, a, c, k, e, d):
+def __unpack(p, a, c, k, e, d, iteration):
+
+    with open('before file'+str(iteration)+'.js', "wb") as filewriter:
+        filewriter.write(str(p))
     while (c > 1):
         c = c -1
         if (k[c]):
-            cc=c
-            aa=str(__itoaNew(cc, a))
-            p = re.sub('\\b' + aa +'\\b', k[c], p)           
+            aa=str(__itoaNew(c, a))
+            #re.sub('\\b' + aa +'\\b', k[c], p) THIS IS Bloody slow!
+            p=findAndReplaceWord(p,aa,k[c])
+
+            
+    with open('after file'+str(iteration)+'.js', "wb") as filewriter:
+        filewriter.write(str(p))
     return p
+
+#
+#function equalavent to re.sub('\\b' + aa +'\\b', k[c], p)
+def findAndReplaceWord(source_str, word_to_find,replace_with):
+    splits=None
+    splits=source_str.split(word_to_find)
+    if len(splits)>1:
+        new_string=[]
+        current_index=0
+        for current_split in splits:
+            #print 'here',i
+            new_string.append(current_split)
+            val=word_to_find#by default assume it was wrong to split
+
+            #if its first one and item is blank then check next item is valid or not
+            if current_index==len(splits)-1:
+                val='' # last one nothing to append normally
+            else:
+                if len(current_split)==0: #if blank check next one with current split value
+                    if ( len(splits[current_index+1])==0 and word_to_find[0].lower() not in 'abcdefghijklmnopqrstuvwxyz1234567890_') or (len(splits[current_index+1])>0  and splits[current_index+1][0].lower() not in 'abcdefghijklmnopqrstuvwxyz1234567890_'):# first just just check next
+                        val=replace_with
+                #not blank, then check current endvalue and next first value
+                else:
+                    if (splits[current_index][-1].lower() not in 'abcdefghijklmnopqrstuvwxyz1234567890_') and (( len(splits[current_index+1])==0 and word_to_find[0].lower() not in 'abcdefghijklmnopqrstuvwxyz1234567890_') or (len(splits[current_index+1])>0  and splits[current_index+1][0].lower() not in 'abcdefghijklmnopqrstuvwxyz1234567890_')):# first just just check next
+                        val=replace_with
+                        
+            new_string.append(val)
+            current_index+=1
+        #aaaa=1/0
+        source_str=''.join(new_string)
+    return source_str        
 
 def __itoa(num, radix):
 #    print 'num red',num, radix
