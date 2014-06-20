6d500bf30abfe1eca8db9de85b9ef0ac6bbcc763
Shani-08 <ShahjahanDurrani@hotmail.com>
2014-06-20 17:36:55 +0100

    support for unpack
    support for unpack

--- other/livestreamchanges/default.py
+++ other/livestreamchanges/default.py
@@ -9,6 +9,7 @@ import xbmcplugin
 import xbmcgui
 import xbmcaddon
 import xbmcvfs
+import traceback
 
 from BeautifulSoup import BeautifulStoneSoup, BeautifulSoup, BeautifulSOAP
 try:
@@ -220,6 +221,7 @@ def rmSource(name):
                     b.close()
                     break
         xbmc.executebuiltin("XBMC.Container.Refresh")
+
 
 
 def get_xml_database(url, browse=False):
@@ -799,7 +801,9 @@ def getRegexParsed(regexs, url,cookieJar=None,forCookieJarOnly=False,recursiveCa
         if '$GUID$' in url:
             import uuid
             url=url.replace('$GUID$',str(uuid.uuid1()).upper())
-            
+        if '$get_cookies$' in url:
+            url=url.replace('$get_cookies$',getCookiesString(cookieJar))   
+
         if recursiveCall: return url
         print 'final url',url
         item = xbmcgui.ListItem(path=url)
@@ -808,7 +812,156 @@ def getRegexParsed(regexs, url,cookieJar=None,forCookieJarOnly=False,recursiveCa
         #xbmc.playlist(xbmc.playlist_video).add(url)
         #xbmc.Player().play(item=url)
         xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item)
+
+def get_unpacked( page_value, regex_for_text, iterations=1, total_iteration=1):
+    try:        
+        if page_value.startswith("http"):
+            page_value= getUrl(page_value)
+        print 'page_value',page_value
+        if regex_for_text and len(regex_for_text)>0:
+            page_value=re.compile(regex_for_text).findall(page_value)[0] #get the js variable
+
+        page_value=unpack(page_value,iterations,total_iteration)
+    except: traceback.print_exc(file=sys.stdout)
+    print 'unpacked',page_value
+    return page_value
+
+def unpack(sJavascript,iteration=1, totaliterations=2  ):
+    print 'iteration',iteration
+    if sJavascript.startswith('var _0xcb8a='):
+        aSplit=sJavascript.split('var _0xcb8a=')
+        ss="myarray="+aSplit[1].split("eval(")[0]
+        exec(ss)
+        a1=62
+        c1=int(aSplit[1].split(",62,")[1].split(',')[0])
+        p1=myarray[0]
+        k1=myarray[3]
+        with open('temp file'+str(iteration)+'.js', "wb") as filewriter:
+            filewriter.write(str(k1))
+        #aa=1/0
+    else:
+
+        aSplit = sJavascript.split("rn p}('")
+        print aSplit
         
+        p1,a1,c1,k1=('','0','0','')
+     
+        ss="p1,a1,c1,k1=('"+aSplit[1].split(".spli")[0]+')' 
+        exec(ss)
+    k1=k1.split('|')
+    aSplit = aSplit[1].split("))'")
+#    print ' p array is ',len(aSplit)
+#   print len(aSplit )
+
+    #p=str(aSplit[0]+'))')#.replace("\\","")#.replace('\\\\','\\')
+
+    #print aSplit[1]
+    #aSplit = aSplit[1].split(",")
+    #print aSplit[0] 
+    #a = int(aSplit[1])
+    #c = int(aSplit[2])
+    #k = aSplit[3].split(".")[0].replace("'", '').split('|')
+    #a=int(a)
+    #c=int(c)
+    
+    #p=p.replace('\\', '')
+#    print 'p val is ',p[0:100],'............',p[-100:],len(p)
+#    print 'p1 val is ',p1[0:100],'............',p1[-100:],len(p1)
+    
+    #print a,a1
+    #print c,a1
+    #print 'k val is ',k[-10:],len(k)
+#    print 'k1 val is ',k1[-10:],len(k1)
+    e = ''
+    d = ''#32823
+
+    #sUnpacked = str(__unpack(p, a, c, k, e, d))
+    sUnpacked1 = str(__unpack(p1, a1, c1, k1, e, d,iteration))
+    
+    #print sUnpacked[:200]+'....'+sUnpacked[-100:], len(sUnpacked)
+#    print sUnpacked1[:200]+'....'+sUnpacked1[-100:], len(sUnpacked1)
+    
+    #exec('sUnpacked1="'+sUnpacked1+'"')
+    if iteration>=totaliterations:
+#        print 'final res',sUnpacked1[:200]+'....'+sUnpacked1[-100:], len(sUnpacked1)
+        return sUnpacked1#.replace('\\\\', '\\')
+    else:
+#        print 'final res for this iteration is',iteration
+        return unpack(sUnpacked1,iteration+1)#.replace('\\', ''),iteration)#.replace('\\', '');#unpack(sUnpacked.replace('\\', ''))
+
+def __unpack(p, a, c, k, e, d, iteration):
+
+    #with open('before file'+str(iteration)+'.js', "wb") as filewriter:
+    #    filewriter.write(str(p))
+    while (c > 1):
+        c = c -1
+        if (k[c]):
+            aa=str(__itoaNew(c, a))
+            #re.sub('\\b' + aa +'\\b', k[c], p) THIS IS Bloody slow!
+            p=findAndReplaceWord(p,aa,k[c])
+
+            
+    #with open('after file'+str(iteration)+'.js', "wb") as filewriter:
+    #    filewriter.write(str(p))
+    return p
+
+#
+#function equalavent to re.sub('\\b' + aa +'\\b', k[c], p)
+def findAndReplaceWord(source_str, word_to_find,replace_with):
+    splits=None
+    splits=source_str.split(word_to_find)
+    if len(splits)>1:
+        new_string=[]
+        current_index=0
+        for current_split in splits:
+            #print 'here',i
+            new_string.append(current_split)
+            val=word_to_find#by default assume it was wrong to split
+
+            #if its first one and item is blank then check next item is valid or not
+            if current_index==len(splits)-1:
+                val='' # last one nothing to append normally
+            else:
+                if len(current_split)==0: #if blank check next one with current split value
+                    if ( len(splits[current_index+1])==0 and word_to_find[0].lower() not in 'abcdefghijklmnopqrstuvwxyz1234567890_') or (len(splits[current_index+1])>0  and splits[current_index+1][0].lower() not in 'abcdefghijklmnopqrstuvwxyz1234567890_'):# first just just check next
+                        val=replace_with
+                #not blank, then check current endvalue and next first value
+                else:
+                    if (splits[current_index][-1].lower() not in 'abcdefghijklmnopqrstuvwxyz1234567890_') and (( len(splits[current_index+1])==0 and word_to_find[0].lower() not in 'abcdefghijklmnopqrstuvwxyz1234567890_') or (len(splits[current_index+1])>0  and splits[current_index+1][0].lower() not in 'abcdefghijklmnopqrstuvwxyz1234567890_')):# first just just check next
+                        val=replace_with
+                        
+            new_string.append(val)
+            current_index+=1
+        #aaaa=1/0
+        source_str=''.join(new_string)
+    return source_str        
+
+def __itoa(num, radix):
+#    print 'num red',num, radix
+    result = ""
+    if num==0: return '0'
+    while num > 0:
+        result = "0123456789abcdefghijklmnopqrstuvwxyz"[num % radix] + result
+        num /= radix
+    return result
+	
+def __itoaNew(cc, a):
+    aa="" if cc < a else __itoaNew(int(cc / a),a) 
+    cc = (cc % a)
+    bb=chr(cc + 29) if cc> 35 else str(__itoa(cc,36))
+    return aa+bb
+
+
+def getCookiesString(cookieJar):
+    try:
+        cookieString=""
+        for index, cookie in enumerate(cookieJar):
+            cookieString+=cookie.name + "=" + cookie.value +";"
+    except: pass
+    print 'cookieString',cookieString
+    return cookieString
+
+
 def saveCookieJar(cookieJar,COOKIEFILE):
 	try:
 		complete_path=os.path.join(profile,COOKIEFILE)
