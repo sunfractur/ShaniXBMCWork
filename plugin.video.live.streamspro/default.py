d49366e22cb1c242ecb2e75dece2a21635d1baab
Shani-08 <ShahjahanDurrani@hotmail.com>
2014-08-18 14:21:53 +0100

    added get_packed_iphonetv_url
    added get_packed_iphonetv_url

--- plugin.video.live.streamspro/default.py
+++ plugin.video.live.streamspro/default.py
@@ -925,6 +925,18 @@ def get_leton_rtmp(page_value, referer=None):
 
     ret= 'rtmp://' + str(a) + '.' + str(b) + '.' + str(c) + '.' + str(d) + v;
     return ret
+
+def get_packed_iphonetv_url(page_data):
+	import re,base64,urllib; 
+	s=page_data
+	while '(atob(' in s: 
+		s=re.compile('"(.*?)"').findall(s)[0]; 
+		s=  base64.b64decode(s); 
+		s=urllib.unquote(s); 
+	print s
+	return s
+
+
     
 def get_dag_url(page_data):
     print 'get_dag_url',page_data
